6e9f3f03de0d4b7dfd2b87b23a8fd84aa6718252
BinubBinu <99821624+BinubBinu@users.noreply.github.com>
2022-09-28 10:26:13 +0530

    responsives are created

---
+++ landing-page-holypretty-cosmetics/src/components/Product.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Product = () => {
+  return (
+    <div>
+      <div className="beauty-products">
+        <nav className="nav">Maximize Your Beauty</nav>
+      </div>
+    </div>
+  );
+}
+
+export default Product;
